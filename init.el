cd66ed690948bdc15f04b03713fee1556cefc1e0
LeoDT <leojoy710@gmail.com>
2014-02-07 16:24:04 +0800

    Update init.el

--- init.el
+++ init.el
@@ -1,40 +1,46 @@
-(tool-bar-mode nil)
-(menu-bar-mode t)
-(setq inhibit-startup-message t)
+(tool-bar-mode 0)
+(menu-bar-mode 1)
+(setq inhibit-startup-message 1)
+(setq initial-scratch-message "")
+(column-number-mode 1)
+(display-time-mode 1)
+(show-paren-mode 1)
 
 (setq default-line-spacing 0)
 (setq default-fill-column 90)
 (setq default-major-mode 'text-mode)
 (setq kill-ring-max 200)
-(setq require-final-newline t) 
+(setq require-final-newline 1)
 
-(global-font-lock-mode t)
+(global-font-lock-mode 1)
 
-(transient-mark-mode t)
+(transient-mark-mode 1)
 (setq scroll-margin 5
       scroll-conservatively 10000)
 
-(show-paren-mode t)
+(show-paren-mode 1)
 (setq show-paren-style 'parentheses)
-(setq mouse-yank-at-point t)
+(setq mouse-yank-at-point 1)
 
-(setq visible-bell nil)
+(setq visible-bell 0)
 (fset 'yes-or-no-p 'y-or-n-p)
-(setq resize-mini-windows nil)
-(setq enable-recursive-minibuffers t)
+(setq resize-mini-windows 0)
+(setq enable-recursive-minibuffers 1)
 
-(setq column-number-mode t)
+(setq column-number-mode 1)
 
-(setq-default make-backup-files nil)
-(setq x-select-enable-clipboard t)
-(setq indent-tabs-mode nil)
-(setq tab-always-indent nil)
+(setq-default make-backup-files 0)
+(setq x-select-enable-clipboard 1)
+(setq indent-tabs-mode 0)
+(setq tab-always-indent 0)
 (setq tab-width 4)
-
 
 ;;shortcut
 (global-set-key (kbd "C-t") 'set-mark-command)
 
+;;font
+(set-default-font "Inconsolata 14")
+(set-fontset-font "fontset-default" 'unicode "Microsoft Yahei 12")
 
 ;;packages
 (require 'package)
@@ -43,8 +49,6 @@
 (package-initialize)
 
 (require 'auto-complete)
-;;(add-to-list 'ac-dictionary-directories "/home/leodt/.emacs.d/elpa/auto-complete-20120717/dict/")
-(require 'go-autocomplete)
 (require 'auto-complete-config)
 (ac-config-default)
 
@@ -64,32 +68,15 @@
 
 (require 'js3-mode)
 (add-to-list 'ac-modes 'js3-mode)
+(setq-default js3-auto-indent-p 1)
+(setq-default js3-enter-indents-newline 1)
+(setq-default js3-indent-level 4)
+(setq-default js3-indent-on-enter-key 1)
 
 ;; web dev
-(require 'zencoding-mode)
-(add-hook 'sgml-mode-hook 'zencoding-mode)
+(require 'emmet-mode)
 
-(require 'sanityinc-tomorrow-eighties-theme)
+(projectile-global-mode)
+(setq projectile-enable-caching 1)
 
-(setq sgml-basic-offset 4)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(column-number-mode t)
- '(display-time-mode t)
- '(js3-auto-indent-p t)
- '(js3-enter-indents-newline t)
- '(js3-indent-level 4)
- '(js3-indent-on-enter-key t)
- '(js3-strict-inconsistent-return-warning nil)
- '(show-paren-mode t)
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(require 'base16-eighties-theme)
